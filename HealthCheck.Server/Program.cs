812411856d5966115f855697d001235ce5b5e5e1
Benedictus Jullian Pradana <benedictusjullian1@gmail.com>
2024-09-19 11:08:54 +0700

    implementing custom output message

--- HealthCheck.Server/Program.cs
+++ HealthCheck.Server/Program.cs
@@ -8,7 +8,7 @@ builder
     .Services.AddHealthChecks()
     .AddCheck("ICMP_01", new ICMPHealthCheck("www.ryadel.com", 100))
     .AddCheck("ICMP_02", new ICMPHealthCheck("www.google.com", 100))
-    .AddCheck("ICMP_03", new ICMPHealthCheck($"www.{Guid.NewGuid():N}.com", 100));
+    .AddCheck("ICMP_03", new ICMPHealthCheck($"www.asdasdfasdfsadfsadfs.com", 100));
 builder.Services.AddControllers();
 
 // Configure CORS
